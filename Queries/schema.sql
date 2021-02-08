55605f12c7a76ad620fe838e0e62ee8d812a5505
Mobin A Piracha <mobinapiracha@gmail.com>
2021-02-08 14:46:52 -0600

    fixing errors

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -18,36 +18,37 @@ CREATE TABLE employees (
 	);
 	
 -- Dept_Manager 
-CREATE TABLE dept_manager (
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no VARCHAR(4) NOT NULL, 
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY(dept_no)REFERENCES departments(dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
 -- Salaries 
-CREATE TABLE salaries (
-	emp_no INT NOT NULL, 
-	salary INT NOT NULL,
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 -- Dept_Emp 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
-)
+);
 
 -- Titles 
 CREATE TABLE titles (
@@ -55,8 +56,7 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-)
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
 
 
