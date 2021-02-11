98012e2ac4a2b66b2318cc381cb3379fe148140e
Mobin A Piracha <mobinapiracha@gmail.com>
2021-02-11 14:55:11 -0600

    Complete SQL queries for challenge

--- Queries/Employee_database_challenge.sql
+++ Queries/Employee_database_challenge.sql
@@ -22,3 +22,15 @@ INTO retiring_titles
 FROM unique_titles AS ut 
 GROUP BY title
 ORDER BY "Count" DESC;
+
+-- Create a mentorship eligibility table 
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility 
+FROM employees as e
+JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (birth_date between '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no; 
